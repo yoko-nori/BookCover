4e9f20eae3d0505108874d41fc784f73dc962b32
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-04-26 18:18:08 +0900

    nextjsの不要な初期ファイル削除

--- project/client/app/next.config.js
+++ project/client/app/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
   output: 'standalone',
 }
 
