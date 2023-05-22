ca4ac77c3d2fd3a40a37c50be7db571ad2ca43c6
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-05-22 12:34:33 +0900

    paletteフォルダ jsx styles作成

---
+++ project/client/app/src/features/palette/components/Palette.jsx
@@ -0,0 +1,9 @@
+import styles from '@/features/palette/styles/palette.module.css';
+
+export const Palette = () => {
+  return (
+    <div className={styles.container}>
+      palette
+    </div>
+  );
+}
--- project/client/app/src/pages/index.jsx
+++ project/client/app/src/pages/index.jsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/layouts/Header/Header"
+import { Palette } from "@/features/palette/components/Palette"
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Palette />
     </>
   )
 }
