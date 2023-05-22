c0817226bf06efec182319318e81bf976153525c
yoko-nori <ykinrtk.gamecreation@gmail.com>
2023-05-22 12:35:55 +0900

    SliderBarフォルダ作成

--- project/client/app/src/components/layouts/Header/Header.jsx
+++ project/client/app/src/components/layouts/Header/Header.jsx
@@ -1,7 +1,28 @@
+import Image from "next/image"
+import styles from '@/components/layouts/Header/header.module.css'
+
 export const Header = () => {
   return (
     <>
-      Header
+      <Image
+        src="/images/kyoto.jpg"
+        //src="/images/Headertest.png"
+        alt="CoverAtelier_icon"
+
+        height={100}
+        width={200}
+
+        priority="high"
+      />
+      <header className="header">
+        {/* <img src = {header} alt ="CoverAtelier_icon" className = "CoverAtelier"/> */}
+
+        <ul className={styles.headerlist}>
+          <li className="headeritem01"><a href="">Create</a></li>
+          <li className="headeritem02"><a href="">Help</a></li>
+          <li className="headeritem03"><a href="">Credit</a></li>
+        </ul>
+      </header>
     </>
   );
 };
