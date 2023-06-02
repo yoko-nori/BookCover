c72a84bcccf27885f0af609c93567f1573e8477a
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-06-02 16:50:31 +0900

    画像の表示エラーを解消

--- project/client/app/src/components/layouts/Header/Header.jsx
+++ project/client/app/src/components/layouts/Header/Header.jsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
 export const Header = () => {
   return (
     <>
-      Header
+      <Image
+        src='/coveratelier.png'
+        alt='coveratelier'
+        width={100}
+        height={24}
+        priority
+      />
     </>
   );
 };
