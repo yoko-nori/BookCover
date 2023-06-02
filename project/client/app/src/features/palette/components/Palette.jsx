93649240e9a86d7fb85ceb32f1750980f13deef0
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-06-02 16:40:58 +0900

    palette.jsx作成

--- project/client/app/src/features/palette/components/Palette.jsx
+++ project/client/app/src/features/palette/components/Palette.jsx
@@ -1,9 +1,20 @@
 import styles from '@/features/palette/styles/palette.module.css';
+import { PaletteButton } from './PaletteButton';
 
 export const Palette = () => {
+  const handleButton = () => {
+    console.log('click on PaletteButton');
+  };
+
   return (
     <div className={styles.container}>
-      palette
+      <div className={styles.left_container}>
+        <PaletteButton
+          icon="images/memo.png"
+          onclick={() => handleButton()}
+        />
+      </div>
+      <div className={styles.right_container}></div>
     </div>
   );
-}
+};
